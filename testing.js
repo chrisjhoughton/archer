cade90d0d4cd5f13f9ef3358c807da1c0e338932
Chris Houghton <chris.john.houghton@gmail.com>
2015-11-06 18:16:29 +0000

    move to publish

--- testing.js
+++ testing.js
@@ -2,8 +2,10 @@ var archer = require('./lib');
 
 
 
+console.log(archer);
 
-archer.get('https://us5.api.mailchimp.com/schema/3.0/Lists/Instance.json').spread(function (res, body) {
 
-  console.log(res.statusCode, body);
-});
+// archer.get('https://us5.api.mailchimp.com/schema/3.0/Lists/Instance.json').spread(function (res, body) {
+
+//   console.log(res.statusCode, body);
+// });
